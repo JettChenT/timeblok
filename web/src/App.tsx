05826cc9bda69d5db80f666974e611cb47043738
JettChenT <jettchen12345@gmail.com>
2023-03-29 23:48:42 +0800

    update web editor

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-import * as tb from "timeblok-js"
+import * as tb from "timeblok-js/dist/bundler"
 
 
 function App() {
-  const [leftText, setLeftText] = useState('Left Text')
-  const [rightText, setRightText] = useState('Right Text')
+  const [leftText, setLeftText] = useState('2023-4-1\n9am do stuff')
+  const [rightText, setRightText] = useState('Click compile to compile to ics')
   const handleClick = () => {
     setRightText("Compiling...")
     let timeout = setTimeout(() => {
@@ -19,18 +19,35 @@ function App() {
       setRightText('error')
     } 
   }
+
+  const export_ics = () => {
+    let element = document.createElement('a');
+    let file = new Blob([rightText], {type: 'text/plain'});
+    element.href = URL.createObjectURL(file);
+    element.download = "export.ics";
+    document.body.appendChild(element);
+    element.click();
+  }
+
   return (
-    <div className="container mx-auto w-screen">
-      <h1>Timeblok Playground! (Very early alpha mode)</h1>
+    <div className="container mx-auto text-center h-screen prose">
+      <h1 className='h-1 block'>Timeblok Playground!</h1>
       <p>
         On the left, enter valid timeblok program.
         On the right, the output(in ics format) will be displayed.
       </p>
       <div className="flex">
-        <textarea value={leftText} onChange={(e) => setLeftText(e.target.value)} className="w-1/2" />
-        <textarea value={rightText} onChange={(e) => setRightText(e.target.value)} className="w-1/2" />
+        <div className='w-1/2'>
+          TimeBlok Code <br/>
+          <textarea className='w-full' value={leftText} onChange={(e) => setLeftText(e.target.value)}/>
+        </div>
+        <div className='w-1/2 ml-2'>
+          ICS export <br/>
+          <textarea className='w-full' value={rightText}/>
+        </div>
       </div>
-      <button className="mt-4" onClick={handleClick}>Button</button>
+      <button className="btn btn-sm" onClick={handleClick}>Compile</button>
+      <button className="btn btn-sm ml-2" onClick={export_ics}>Export to ICS</button>
     </div>
  )
 
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"),require("daisyui")],
 }
