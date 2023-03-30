ef6587fb969a74aa14145cae3d3a09193627b00e
JettChenT <jettchen12345@gmail.com>
2023-03-30 17:38:14 +0800

    migrate web to separate timeblok-playground repo

--- web/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), wasm(), topLevelAwait()],
-})
