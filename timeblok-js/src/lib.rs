1af0384f81e517447d64faef943bfeaa7d0819eb
JettChenT <jettchen12345@gmail.com>
2023-04-07 22:47:39 +0800

    new version: better support for WASM

--- timeblok-js/src/lib.rs
+++ timeblok-js/src/lib.rs
@@ -1,8 +1,21 @@
 use timeblok::ir::ExactDateTime;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_test::*;
-
 mod utils;
+extern crate web_sys;
+
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
+macro_rules! log_debug {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!("{:?}", $( $t )* ).into());
+    }
+}
+
 
 #[wasm_bindgen]
 pub fn compile(source: &str, base_time: i64) -> Option<String> {
@@ -18,6 +31,16 @@ pub fn compile_with_basedate(source: &str, year: i32, month: u32, day: u32) -> O
         Ok(ics) => Some(ics),
         Err(_) => None,
     }
+}
+
+#[wasm_bindgen]
+pub fn compile_verbose(source: &str, year: i32, month: u32, day: u32) -> Option<String> {
+    let records = timeblok::tb_to_records(&source.to_string()).ok()?;
+    log_debug!(records);
+    let resolved = timeblok::records_to_resolved(records, ExactDateTime::from_ymd_hms(year, month, day,0,0,0)).ok()?;
+    log_debug!(resolved);
+    let ics = timeblok::resolved_to_ical(resolved).ok()?;
+    Some(ics)
 }
 
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
